d28ed56ead6d3e8ef1ff71ebc7613e7f0f06f9e4
Aki Kuivas <a-p.kvs@pm.me>
2022-09-18 21:30:24 +0300

    vaihettu dropdownlista ja suunniteltu hieman ulkonäköä.

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import RadioForm from 'react-native-simple-radio-button';
 import { useState } from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { useCallback } from 'react';
 
 export default function App() {
   const [weight, setWeight] = useState(0);
@@ -10,12 +13,15 @@ export default function App() {
   const [bloodLevel, setBloodLevel] = useState(0);
   const [gender, setGender] = useState('male');
 
+  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
+
   const genders = [
     {label: 'Male', value: 'male'},
     {label: 'Female', value: 'female'},
-  ]
+  ];
 
-  const times = [
+  const [times, setTimes] = useState([
     {label: '1h', value: 1},
     {label: '2h', value: 2},
     {label: '3h', value: 3},
@@ -26,9 +32,9 @@ export default function App() {
     {label: '8h', value: 8},
     {label: '9h', value: 9},
     {label: '10h', value: 10},
-  ]
+  ]);
 
-  const bottles = [
+  const [bottles, setBottles] = useState([
     {label: '1kpl', value: 1},
     {label: '2kpl', value: 2},
     {label: '3kpl', value: 3},
@@ -38,8 +44,8 @@ export default function App() {
     {label: '7kpl', value: 7},
     {label: '8kpl', value: 8},
     {label: '9kpl', value: 9},
-    {label: '1kpl', value: 10},
-  ]
+    {label: '10kpl', value: 10},
+  ]);
 
   function calculate() {
       let result = 0
@@ -47,6 +53,10 @@ export default function App() {
       let grams = 0
       let burning = 0
       let gramsLeft = 0
+
+      console.log(bottle, "bottle")
+      console.log(time, "time")
+      console.log(weight, "weight")
 
       litres = bottle * 0.33
       grams = litres * 8 * 4.5  
@@ -64,31 +74,50 @@ export default function App() {
         setBloodLevel(0)
       }      
       //testitesti
-  }
+    }
+      const onOpen = useCallback(() => {
+        setOpen1(false);
+      }, []);
+      const onOpen1 = useCallback(() => {
+        setOpen(false);
+      }, []);
+  
 
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
     <View style={styles.container}>
       <Text>Weight:</Text>
       <TextInput onChangeText={text => setWeight(text.replace(',','.'))}  style={styles.field} keyboardType='decimal-pad'placeholder='Syötä paino'/>
 
       <Text>Botles:</Text>
-      <Picker selectedValue={bottle} onValueChange={(itemValue) => setBottle(itemValue)} >
-      {
-        bottles.map((bottles, index) => {
-          return <Picker.Item key={index} label={bottles.label} value={bottles.value} />
-      })
-    }  
-    </Picker>
+      <View style={styles.DropDownPicker}>
+        <DropDownPicker
+        open={open}
+        onOpen={onOpen}
+        value={bottle}
+        items={bottles}
+        setOpen={setOpen}
+        setValue={setBottle}
+        setItems={setBottles}
 
-      <Text>Time:</Text>     
-      <Picker selectedValue={time} onValueChange={(itemValue) => setTime(itemValue)} >
-      {
-        //ylemmässä pikkerissä on aaltosulut jotka vaativat return käskyn, alemmassa on normaalit sulut
-        times.map((times, index) => (
-          <Picker.Item key={index} label={times.label} value={times.value} />
-        ))
-    } 
-    </Picker>
+        dropDownStyle={{backgroundColor: '#00ffff'}}
+        itemStyle={{justifyContent: 'flex-start|flex-end|center'}}
+        
+        />
+      </View>
+
+      <Text>Time:</Text>    
+      <View style={styles.DropDownPicker}>
+        <DropDownPicker
+        open={open1}
+        onOpen={onOpen1}
+        value={time}
+        items={times}
+        setOpen={setOpen1}
+        setValue={setTime}
+        setItems={setTimes}
+        />
+      </View>
 
       <Text>Gender</Text>
       <RadioForm
@@ -104,15 +133,23 @@ export default function App() {
       <Button title="Calculate" onPress={calculate}/>
   
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 100,
+    backgroundColor: '#00ffff',
+    marginTop: 50,
     marginLeft: 10,
     marginRight: 10,
+  },
+  DropDownPicker:{
+    width: 150,
+    flex: 1,
+    backgroundColor: '#00ffff',
+    marginTop: 10,
+    zIndex: 10,
   },
 });
